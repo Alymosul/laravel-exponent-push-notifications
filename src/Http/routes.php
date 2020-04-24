1d1c86e6dde0a299f82ad83ef0f4214f434431c6
TomaszNiedzielski <64227222+TomaszNiedzielski@users.noreply.github.com>
2020-04-24 10:35:36 +0200

    Update routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['prefix' => 'api/exponent/devices', 'middleware' => ['auth:api', 'bindings']], function () {
+Route::group(['prefix' => 'api/exponent/devices', 'middleware' => ['jwt.verify', 'bindings']], function () {
     Route::post('subscribe', [
         'as'    =>  'register-interest',
         'uses'  =>  'NotificationChannels\ExpoPushNotifications\Http\ExpoController@subscribe',
